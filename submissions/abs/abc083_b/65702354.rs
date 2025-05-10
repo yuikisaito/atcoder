48345d73d39f9374e8eaca395e55d8c4b0c1543a
Yuiki Saito <yuikisaito@ysaito.win>
2025-05-10 17:55:15 +0000

    submit #65702354 [WA]

---
+++ submissions/abs/abc083_b/65702354.rs
@@ -0,0 +1,16 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: u32, a: u32, b: u32,
+    }
+
+    let mut num = 0;
+    for i in 0..n {
+        let keta = i.to_string().chars().map(|c| c.to_digit(10).unwrap()).sum();
+        if a <= keta && keta <= b {
+            num += i;
+        }
+    }
+    print!("{}", num);
+}
