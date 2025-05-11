fa40b5d30d9ec22dad1ff41f33d4aba4e65d12c0
Yuiki Saito <yuikisaito@ysaito.win>
2025-05-11 14:02:41 +0000

    submit #65722407 [WA]

---
+++ submissions/abc060/abc060_b/65722407.rs
@@ -0,0 +1,19 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    }
+
+    let mut n = 0;
+    for i in 1..=b {
+        n += a ^ i;
+        if n % b == c {
+            println!("YES");
+            return;
+        }
+    }
+    println!("NO")
+}
