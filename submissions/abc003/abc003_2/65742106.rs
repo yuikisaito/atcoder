390c0abcaac980febd553e98533acd1a7a32343a
Yuiki Saito <yuikisaito@ysaito.win>
2025-05-12 13:39:18 +0000

    submit #65742106 [WA]

---
+++ submissions/abc003/abc003_2/65742106.rs
@@ -0,0 +1,44 @@
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    for i in 0..s.len() {
+        let si = s[i];
+        let ti = t[i];
+        if si != ti {
+            let sis = si.to_string();
+            let tis = ti.to_string();
+            if sis == "@"
+                && (tis == "a"
+                    || tis == "t"
+                    || tis == "c"
+                    || tis == "o"
+                    || tis == "d"
+                    || tis == "e"
+                    || tis == "r"
+                    || tis == "@")
+            {
+                break;
+            }
+            if tis == "@"
+                && (sis == "a"
+                    || sis == "t"
+                    || sis == "c"
+                    || sis == "o"
+                    || sis == "d"
+                    || sis == "e"
+                    || sis == "r"
+                    || sis == "@")
+            {
+                break;
+            }
+            println!("You will lose");
+            return;
+        }
+    }
+    println!("You can win")
+}
