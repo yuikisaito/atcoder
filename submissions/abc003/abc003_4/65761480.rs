c70227df391f6c64e9a4254c943cdb75a351cf17
Yuiki Saito <yuikisaito@ysaito.win>
2025-05-13 11:55:28 +0000

    submit #65761480 [WA]

---
+++ submissions/abc003/abc003_4/65761480.rs
@@ -0,0 +1,20 @@
+use itertools::Itertools;
+use proconio::input;
+
+fn main() {
+    input! {
+        r: usize,
+        c: usize,
+        x: usize,
+        y: usize,
+        d: usize,
+        l: usize,
+    }
+
+    let xy_pattern = ((r - x + 1) * (c - y + 1)) % 1000000007;
+
+    let dl_pattern = ((0..x * y).combinations(d + l).count() % 1000000007)
+        * ((0..d + l).combinations(d).count() % 1000000007);
+
+    println!("{}", xy_pattern * dl_pattern)
+}
