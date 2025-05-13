cb8da355e204aaee7f1db8dfddd679995e450865
Yuiki Saito <yuikisaito@ysaito.win>
2025-05-13 12:24:41 +0000

    submit #65762187 [WA]

---
+++ submissions/abc003/abc003_4/65762187.rs
@@ -0,0 +1,20 @@
+use itertools::{Itertools, iproduct};
+use proconio::input;
+
+const MOD: usize = 1000000007;
+
+fn main() {
+    input! {
+        r: usize,
+        c: usize,
+        x: usize,
+        y: usize,
+        d: usize,
+        l: usize,
+    }
+
+    let xy_pattern = iproduct!(0..=r - x, 0..=c - y).count();
+
+    let dl_pattern = (0..x * y).combinations(d + l).count() * (0..d + l).combinations(d).count();
+    println!("{}", xy_pattern * dl_pattern % MOD)
+}
