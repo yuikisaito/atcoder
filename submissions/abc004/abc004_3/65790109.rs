dc3db2d9b637798f84737680ab47f0046ed15f7d
Yuiki Saito <yuikisaito@ysaito.win>
2025-05-14 14:11:52 +0000

    submit #65790109 [TLE]

---
+++ submissions/abc004/abc004_3/65790109.rs
@@ -0,0 +1,19 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+    }
+
+    let mut cards: Vec<usize> = (1..=6).collect();
+    for i in 0..=n - 1 {
+        let i_mod_5 = i % 5;
+        cards.swap(i_mod_5, i_mod_5 + 1);
+    }
+    let result = cards
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join("");
+    println!("{}", result)
+}
