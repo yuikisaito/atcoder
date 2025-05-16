52a10db6fe17efd9203c4c0bf65d6346d0ce36ca
Yuiki Saito <yuikisaito@ysaito.win>
2025-05-16 14:05:20 +0000

    submit #65830486 [TLE]

---
+++ submissions/abc006/abc006_3/65830486.rs
@@ -0,0 +1,21 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+    }
+
+    for adult in 0..=n {
+        for elderly in 0..=n - adult {
+            let baby = n - adult - elderly;
+            let legs = 2 * adult + 3 * elderly + 4 * baby;
+            if legs == m {
+                println!("{} {} {}", adult, elderly, baby);
+                return;
+            }
+        }
+    }
+
+    println!("-1 -1 -1");
+}
