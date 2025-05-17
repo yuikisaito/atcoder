1e82245735cd8fcc7d04c392b6eefcb572149565
Yuiki Saito <yuikisaito@ysaito.win>
2025-05-17 12:54:34 +0000

    submit #65901571 [WA]

---
+++ submissions/abc406/abc406_b/65901571.rs
@@ -0,0 +1,25 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let mut calculator = 1;
+    for i in a {
+        if i >= 10usize.pow(k as u32) {
+            calculator = 1;
+            continue;
+        }
+        let result = calculator * i;
+        calculator = if result >= 10usize.pow(k as u32) {
+            1
+        } else {
+            result
+        };
+    }
+
+    println!("{}", calculator);
+}
