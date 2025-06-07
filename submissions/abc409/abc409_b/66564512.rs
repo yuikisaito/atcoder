54e606d17bdc713ebd7086004adabb9dcb7707cd
Yuiki Saito <yuikisaito@ysaito.win>
2025-06-07 13:08:14 +0000

    submit #66564512 [TLE]

---
+++ submissions/abc409/abc409_b/66564512.rs
@@ -0,0 +1,21 @@
+use itertools::Itertools;
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let result = (0..=*a.iter().max().unwrap())
+        .map(|i| {
+            if i <= a.iter().filter(|ai| ai >= &&i).count() {
+                i
+            } else {
+                0
+            }
+        })
+        .max()
+        .unwrap();
+    println!("{}", result);
+}
