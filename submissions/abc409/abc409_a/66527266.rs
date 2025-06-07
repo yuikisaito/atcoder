0e2a170452a3f40b4db7fd032ce1480272695242
Yuiki Saito <yuikisaito@ysaito.win>
2025-06-07 12:16:35 +0000

    submit #66527266 [AC]

---
+++ submissions/abc409/abc409_a/66527266.rs
@@ -0,0 +1,15 @@
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        _: usize,
+        t: Chars,
+        a: Chars,
+    }
+
+    let result = t
+        .iter()
+        .zip(a.iter())
+        .any(|(ti, ai)| *ti == 'o' && *ai == 'o');
+    println!("{}", if result { "Yes" } else { "No" });
+}
