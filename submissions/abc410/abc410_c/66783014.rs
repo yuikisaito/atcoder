5f5d4ddc792fd9a624b4f0fabeabb2459dd196b1
Yuiki Saito <yuikisaito@ysaito.win>
2025-06-14 13:54:40 +0000

    submit #66783014 [RE]

---
+++ submissions/abc410/abc410_c/66783014.rs
@@ -0,0 +1,29 @@
+use itertools::Itertools;
+use proconio::input;
+use text_io::read;
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+    }
+
+    let mut digits = (1..=n).collect_vec();
+    for _ in 0..q {
+        let input: String = read!("{}\n");
+        let mut iter = input.trim().split_whitespace().map(|s| s.parse::<usize>().unwrap());
+        let a = iter.next();
+        let b = iter.next();
+        let c = iter.next();
+        match a.unwrap() {
+            1 => digits[b.unwrap() - 1] = c.unwrap(),
+            2 => println!("{}", digits[b.unwrap() - 1]),
+            3 => {
+                let head = digits.get(0..b.unwrap()).unwrap().to_vec();
+                digits = digits.get(b.unwrap()..).unwrap().to_vec();
+                digits.extend(head);
+            },
+            _ => {}
+        }
+    }
+}
