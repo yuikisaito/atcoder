22c5c295904a1ce6d297a353fb05c0135e76523d
Yuiki Saito <yuikisaito@ysaito.win>
2025-09-14 13:02:03 +0000

    submit #69335357 [RE]

---
+++ submissions/abc423/abc423_c/69335357.rs
@@ -0,0 +1,19 @@
+use proconio::input;
+
+fn main() {
+  input! {
+    n: usize,
+    r: usize,
+    l: [usize; n],
+  }
+
+  if let Some(left_option) = l.iter().position(|&li| li == 0) {
+    let left = left_option.min(r);
+    let right = l.iter().rposition(|&li| li == 0).unwrap().max(r - 1);
+    let range = right - left + 1;
+    let count = l[left..=right].iter().filter(|&&li| li == 1).count();
+    println!("{}", count + range);
+  } else {
+    println!("0");
+  }
+}
