9d6076f16d3a3824168834a10d802bfa91a4f417
Yuiki Saito <yuikisaito@ysaito.win>
2025-09-14 12:36:35 +0000

    submit #69324157 [AC]

---
+++ submissions/abc423/abc423_b/69324157.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+fn main() {
+  input! {
+    n: usize,
+    l: [usize; n],
+  }
+
+  let left = l.iter().position(|&li| li == 1).unwrap_or(n - 1) + 1;
+  let right = ((n - 1) - l.iter().rposition(|&li| li == 1).unwrap_or(n - 1)) + 1;
+
+  println!("{}", (n + 1) - (left + right).min(n + 1));
+}
