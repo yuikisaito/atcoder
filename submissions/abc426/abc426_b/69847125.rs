c66d37d8b75bef52d2a50e884d564f0d023e1d03
Yuiki Saito <yuikisaito@ysaito.win>
2025-10-04 12:24:15 +0000

    submit #69847125 [AC]

---
+++ submissions/abc426/abc426_b/69847125.rs
@@ -0,0 +1,18 @@
+use std::collections::HashSet;
+
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+  input! {
+    s: Chars,
+  }
+
+  let types: HashSet<&char> = s.iter().collect();
+  for t in types {
+    if s.iter().filter(|&si| si == t).count() == 1 {
+      println!("{}", t);
+      return;
+    }
+  }
+}
