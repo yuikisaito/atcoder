92bc05c4b1b57cbffd199f400ba9014f0894ad6c
Yuiki Saito <yuikisaito@ysaito.win>
2025-10-25 13:40:16 +0000

    submit #70460990 [TLE]

---
+++ submissions/abc429/abc429_c/70460990.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+
+use proconio::input;
+
+fn main() {
+  input! {
+    n: usize,
+    a: [usize; n],
+  }
+
+  let mut nums = HashMap::new();
+  for k in a {
+    if let Some(v) = nums.get(&k) {
+      nums.insert(k, v + 1);
+    } else {
+      nums.insert(k, 1usize);
+    }
+  }
+
+  let mut two_from_two = HashMap::new();
+  for &k in nums.keys() {
+    let v = *nums.get(&k).unwrap();
+    if v >= 2 {
+      two_from_two.insert(k, v * (v - 1) / 2);
+    } else {
+      two_from_two.remove(&k);
+    }
+  }
+
+  let keys_len = nums.keys().len();
+  if keys_len < 2 {
+    println!("0");
+    return;
+  }
+
+  let two_cnt = two_from_two.len();
+  let one_cnt = keys_len - two_cnt;
+
+  let mut one_two = 0;
+  for (_, v) in two_from_two.clone() {
+    one_two += v * one_cnt;
+  }
+
+  let mut two_two = 0;
+  for ka in two_from_two.keys() {
+    for kb in two_from_two.keys() {
+      if ka != kb {
+        two_two += two_from_two.get(ka).unwrap() * nums.get(kb).unwrap();
+      }
+    }
+  }
+
+  println!("{}", one_two + two_two);
+}
