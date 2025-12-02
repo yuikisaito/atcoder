3e7aa05b6c921c173443a79f91ad273c5aad925d
Yuiki Saito <yuikisaito@ysaito.win>
2025-12-02 16:35:09 +0000

    submit #71402919 [WA]

---
+++ submissions/math-and-algorithm/math_and_algorithm_h/71402919.rs
@@ -0,0 +1,18 @@
+use proconio::input;
+
+fn main() {
+  input! {
+    n: usize,
+    s: usize,
+  }
+
+  if s <= n + 1 {
+    let k = s - 1;
+    println!("{}", k * (k + 1) / 2);
+  } else {
+    let rect = n * (s - n - 1);
+    let tri = n * (n + 1) / 2;
+    let over_tri = (s - n - 1) * ((s - n - 1) + 1) / 2;
+    println!("{}", rect + tri - over_tri);
+  }
+}
