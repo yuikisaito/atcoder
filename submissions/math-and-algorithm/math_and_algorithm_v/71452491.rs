1e2044212f0dee70d96effc52a2b870587400886
Yuiki Saito <yuikisaito@ysaito.win>
2025-12-05 19:20:53 +0000

    submit #71452491 [WA]

---
+++ submissions/math-and-algorithm/math_and_algorithm_v/71452491.rs
@@ -0,0 +1,24 @@
+use proconio::input;
+
+fn main() {
+  input! {
+      n: usize,
+      a: [usize; n],
+  }
+
+  let mut counter = vec![0; 100000];
+
+  for x in a {
+    counter[x] += 1;
+  }
+
+  let mut ans = 0;
+
+  for i in 1..=50000 {
+    ans += counter[i] * counter[100000 - i];
+  }
+
+  ans += counter[50000] * (counter[50000] - 1) / 2;
+
+  println!("{}", ans);
+}
