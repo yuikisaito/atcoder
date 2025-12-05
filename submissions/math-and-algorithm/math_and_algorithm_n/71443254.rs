ce0e9158f2dff140c64930c5b2adf24aa3590ff8
Yuiki Saito <yuikisaito@ysaito.win>
2025-12-05 07:03:13 +0000

    submit #71443254 [AC]

---
+++ submissions/math-and-algorithm/math_and_algorithm_n/71443254.rs
@@ -0,0 +1,22 @@
+use itertools::Itertools;
+use num_integer::sqrt;
+use proconio::input;
+
+fn main() {
+  input! {
+    mut n: usize,
+  }
+
+  let mut result = Vec::new();
+  for i in 2..=sqrt(n) {
+    while n % i == 0 {
+      result.push(i);
+      n /= i;
+    }
+  }
+  if n != 1 {
+    result.push(n);
+  }
+
+  println!("{}", result.into_iter().join(" "));
+}
