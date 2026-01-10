a5ee6f762af82c397d4cae3104d42065520a191d
Yuiki Saito <yuikisaito@ysaito.win>
2026-01-10 13:37:00 +0000

    submit #72381202 [TLE]

---
+++ submissions/abc440/abc440_c/72381202.rs
@@ -0,0 +1,36 @@
+use text_io::read;
+
+fn main() {
+  let t: usize = read!();
+
+  for _ in 0..t {
+    let n: usize = read!();
+    let w: usize = read!();
+    let mut c = Vec::<usize>::new();
+    for _ in 0..n {
+      c.push(read!());
+    }
+
+    let sum: usize = c.iter().sum();
+
+    println!(
+      "{}",
+      if w > n {
+        0
+      } else {
+        (0..w.min(n))
+          .map(|x| {
+            let nuru: usize = c
+              .iter()
+              .enumerate()
+              .filter(|&(a, &_b)| (a + x) % (2 * w) < w)
+              .map(|(_a, &b)| b)
+              .sum();
+            nuru.min(sum - nuru)
+          })
+          .min()
+          .unwrap()
+      }
+    )
+  }
+}
