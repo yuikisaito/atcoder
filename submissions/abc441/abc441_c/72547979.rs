bf360df95602ec472901e9ca6cc5dde4b6e3c945
Yuiki Saito <yuikisaito@ysaito.win>
2026-01-17 13:07:52 +0000

    submit #72547979 [WA]

---
+++ submissions/abc441/abc441_c/72547979.rs
@@ -0,0 +1,27 @@
+use itertools::Itertools;
+use proconio::input;
+
+fn main() {
+  input! {
+    n: u64,
+    k: u64,
+    x: u64,
+    a: [u64; n],
+  }
+
+  let mut sum = 0;
+  let mut i = 0;
+  let mut sorted = a.into_iter().sorted();
+  while sum < x && i < k {
+    sum += sorted.next().unwrap();
+    i += 1;
+  }
+  println!(
+    "{}",
+    if sum < x {
+      -1
+    } else {
+      (i as f64 * n as f64 / k as f64).ceil() as i64
+    }
+  );
+}
