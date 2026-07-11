1fb02598f68866b1c905da683b9423cfad0cdda1
Yuiki Saito <yuikisaito@ysaito.win>
2026-07-11 12:38:54 +0000

    submit #77379454 [AC]

---
+++ submissions/abc466/abc466_c/77379454.rs
@@ -0,0 +1,28 @@
+use text_io::read;
+
+fn main() {
+  let n: usize = read!();
+
+  let mut ans = 0;
+
+  let mut a = 1;
+  let mut b = 2;
+
+  loop {
+    if b > n {
+      break;
+    }
+    println!("? {} {}", a, b);
+    let res: String = read!();
+    if res == "Yes" {
+      ans += b - a;
+      b += 1;
+    } else if a + 1 == b {
+      b += 1;
+    } else {
+      a += 1;
+    }
+  }
+
+  println!("! {}", ans);
+}
