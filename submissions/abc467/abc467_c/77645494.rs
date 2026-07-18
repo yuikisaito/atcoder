afd093c57133cab6291d27294c08709d89cd4302
Yuiki Saito <yuikisaito@ysaito.win>
2026-07-18 13:34:34 +0000

    submit #77645494 [AC]

---
+++ submissions/abc467/abc467_c/77645494.rs
@@ -0,0 +1,33 @@
+use itertools::izip;
+use proconio::input;
+
+fn main() {
+  input! {
+    n: usize,
+    _m: usize,
+    a: [usize; n],
+    b: [usize; n - 1],
+  }
+
+  println!(
+    "{}",
+    [true, false]
+      .into_iter()
+      .map(|mut rev| {
+        let mut ans = rev as usize;
+        for (x, y) in izip!(a.windows(2), b.iter()) {
+          let sum: usize = x.iter().sum();
+          if (sum + rev as usize) % 2 != *y {
+            rev = true;
+            ans += 1;
+          } else {
+            rev = false;
+          }
+        }
+
+        ans
+      })
+      .min()
+      .unwrap()
+  );
+}
