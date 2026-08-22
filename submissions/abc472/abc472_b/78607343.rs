173dc397294ce33e5f38297fd9d104032bd549a6
Yuiki Saito <yuikisaito@ysaito.win>
2026-08-22 12:22:23 +0000

    submit #78607343 [WA]

---
+++ submissions/abc472/abc472_b/78607343.rs
@@ -0,0 +1,30 @@
+use std::collections::BinaryHeap;
+
+use itertools::Itertools;
+use proconio::input;
+
+fn main() {
+  input! {
+    n: usize,
+    l: [usize; n],
+  }
+
+  let kirikomi_pos = l
+    .into_iter()
+    .scan(0, |state, i| {
+      *state += i;
+      Some(*state)
+    })
+    .collect_vec();
+  let len = kirikomi_pos[n - 1];
+  let mut bh = BinaryHeap::from(kirikomi_pos);
+  let mut prev = usize::MAX;
+  while let Some(i) = bh.pop() {
+    let d = len.abs_diff(i * 2);
+    if prev < d {
+      println!("{}", prev);
+      return;
+    }
+    prev = d;
+  }
+}
